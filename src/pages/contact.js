e80afa9ff014eda6a75187a22ffe057640e5ec17
Laurence <ltaylorapp@gmail.com>
2019-08-13 22:38:30 +0100

    Added CV page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,7 @@ import Header from "../components/header"
 
 export default () => (
   <div>
-    <Header headerText="Contact" />
+    <Header headerText='Contact' />
     <p>Send us a message!</p>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,11 @@ export default () => (
     <Header headerText='Laurence Taylor' />
     <p>
       <Container>
-        I'm a London based full-stack developer and former fraud analyst.
-        I graduated from <a href='https://www.qmul.ac.uk/'>Queen Mary University of London</a> in 2017 and recently completed <a href='https://makers.tech/'>Makers</a>, a highly selective coding bootcamp in London.
-        In my spare time I enjoy playing five-a-side football, reading, and practicing guitar.
+        <p>I'm a London based full-stack developer and former fraud analyst.
+        I graduated from <a href='https://www.qmul.ac.uk/' target='_blank'>Queen Mary University of London</a> in 2017 and recently completed <a href='https://makers.tech/' target='_blank'>Makers</a>, a highly selective coding bootcamp in London.
+        In my spare time I enjoy playing five-a-side football, reading, and practicing guitar.</p>
+
+        <p>I am available immediately as a junior software developer.</p>
       </Container>
     </p>
   </div>

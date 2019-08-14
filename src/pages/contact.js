19013a9490d130317bd0508413b7bab1e4e5aef3
Laurence <ltaylorapp@gmail.com>
2019-08-14 23:03:50 +0100

    Added contact form, updated link hover

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,27 @@
 import React from "react"
 import Header from "../components/header"
+import Container from "../components/container"
 
 export default () => (
   <div>
     <Header headerText='Contact' />
-    <p>Send us a message!</p>
+    <p>
+      <Container>
+      <center>
+        <form method='post' action='https://formspree.io/contact@laurencetaylor.co.uk'>
+          Email
+          <br/>
+          <input type='email' name='_replyto' id='email-box' />
+          <br/>
+          <br/>
+          Message
+          <br/>
+          <textarea name='body' id='body-box' />
+          <br/>
+          <input type='submit' value='Send' />
+        </form>
+      </center>
+      </Container>
+    </p>
   </div>
 )

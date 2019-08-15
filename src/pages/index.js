dae6ebf6d7c205dd9557091636dd7e7aabc37805
Laurence <ltaylorapp@gmail.com>
2019-08-15 11:44:47 +0100

    Added photo, made it so text wraps around photo and it doesn't overflow container

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,14 @@ export default () => (
     <Header headerText='Laurence Taylor' />
     <p>
       <Container>
-        <p>I'm a London based full-stack developer and former fraud analyst.
-        I graduated from <a href='https://www.qmul.ac.uk/' target='_blank'>Queen Mary University of London</a> in 2017 and recently completed <a href='https://makers.tech/' target='_blank'>Makers</a>, a highly selective coding bootcamp in London.
-        In my spare time I enjoy playing five-a-side football, reading, and practicing guitar.</p>
+        <div className='clearfix'>
+          <img src={ require('../imgs/laurence-taylor.jpg') } id='laurence-taylor' />
+          <p>I'm a London based full-stack developer and former fraud analyst.
+          I graduated from <a href='https://www.qmul.ac.uk/' target='_blank'>Queen Mary University of London</a> in 2017 and recently completed <a href='https://makers.tech/' target='_blank'>Makers</a>, a highly selective coding bootcamp in London.
+          In my spare time I enjoy playing five-a-side football, reading, and practicing guitar.</p>
 
-        <p>I am available immediately as a junior software developer.</p>
+          <p>I am available immediately as a junior software developer.</p>
+        </div>
       </Container>
     </p>
   </div>

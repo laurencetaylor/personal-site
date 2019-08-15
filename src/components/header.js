57145bfebf20c67085381ff77d038209db10602d
Laurence <ltaylorapp@gmail.com>
2019-08-15 22:46:05 +0100

    Updated header styling

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,6 @@ export default props => (
   <div id='navbar'>
   <link rel="stylesheet" href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'/>
     <h1>{props.headerText}</h1>
-    <span id={props.headerId}>●</span> <Link to='/' id='cyan-text'>HOME</Link> | <Link to='/cv' id='magenta-text'>CV</Link> | <Link to='/contact' id='yellow-text'>CONTACT</Link> | <a href='https://laurencetaylor.carbonmade.com/' target='_blank' id='key-text'>DESIGN</a> <span id={props.headerId}>●</span>
+    <Link to='/' id='cyan-text'>HOME</Link> <span className={props.headerId}>|</span> <Link to='/cv' id='magenta-text'>CV</Link> <span className={props.headerId}>|</span> <Link to='/contact' id='yellow-text'>CONTACT</Link> <span className={props.headerId}>|</span> <a href='https://laurencetaylor.carbonmade.com/' target='_blank' id='key-text'>DESIGN</a>
   </div>
 )

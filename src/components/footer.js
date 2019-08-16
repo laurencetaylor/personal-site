f00b25782130021b35e512021f626ff3b3ec0df9
Laurence <ltaylorapp@gmail.com>
2019-08-16 12:14:01 +0100

    Removed unnecessary files, rounded profile picture, updated footer, added custom favicon

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,6 @@ import React from "react"
 export default props => (
   <div id='footer'>
     <link rel="stylesheet" href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'/>
-    <a href='https://github.com/laurencetaylor' target='_blank' class='fa fa-github'></a> | <a href='https://www.linkedin.com/in/laurencetaylorlondon/' target='_blank' class='fa fa-linkedin'></a>
+    <a href='http://www.laurencetaylor.co.uk/' target='_blank'>old site</a> | <a href='https://github.com/laurencetaylor' target='_blank' class='fa fa-github'></a> | <a href='https://www.linkedin.com/in/laurencetaylorlondon/' target='_blank' class='fa fa-linkedin'></a> | <a href='https://www.codewars.com/users/LaurenceTaylor' target='_blank'>codewars</a>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default () => (
         <img src={ require('../imgs/laurence-taylor.jpg') } id='laurence-taylor' />
         <p>I'm a London based full-stack developer and former fraud analyst.
         I graduated from <a href='https://www.qmul.ac.uk/' target='_blank'>Queen Mary University of London</a> in 2017 and recently completed <a href='https://makers.tech/' target='_blank'>Makers</a>, a highly selective coding bootcamp in London.
-        In my spare time I enjoy playing five-a-side football, reading, and practicing guitar.</p>
+        In my spare time I enjoy playing five-a-side football, reading, and practicing guitar. I also like hoarding domain names.</p>
 
         <p>I am available immediately as a junior software developer.</p>
       </div>

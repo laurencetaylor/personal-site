1781dcbe036aaf7ea3e62f00262119d17bbbc820
Laurence Taylor <ltaylorapp@gmail.com>
2019-08-31 23:02:56 +0100

    Minor change to personal profile

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,10 +11,10 @@ export default () => (
       <div className='clearfix'>
         <img src={ require('../imgs/laurence-taylor.jpg') } id='laurence-taylor-picture' alt='Laurence Taylor' />
         <p>I'm a London based full-stack developer and former fraud analyst.
-        I graduated from <a href='https://www.qmul.ac.uk/' target='_blank' rel='noopener noreferrer'>Queen Mary University of London</a> in 2017 and recently completed <a href='https://makers.tech/' target='_blank' rel='noopener noreferrer'>Makers</a>, a highly selective coding bootcamp in London.
+        I graduated from <a href='https://www.qmul.ac.uk/' target='_blank' rel='noopener noreferrer'>Queen Mary University of London</a> in 2017 and recently completed <a href='https://makers.tech/' target='_blank' rel='noopener noreferrer'>Makers</a>, a selective coding bootcamp in London.
         In my spare time I enjoy playing five-a-side football, reading, and practicing guitar. I also like hoarding domain names.</p>
 
-        <p>I am available immediately as a junior software developer.</p>
+        <p>I am available immediately as a junior software engineer.</p>
       </div>
     </Container>
     <Footer />
